601b6570d347d6226d01b03e8feaab705a8dfe24
Madannagari Raju <madannagariraju@gmail.com>
2024-10-21 20:36:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// console.log("hello world")
+
+// var name = "raju";
+// var age = 22;
+// var city = "hyd"
+
+// console.log(`My Name is ${name}\n age ${age}\n lives in ${city}`)
+
+
+
+// var tutorial = "java script"
+// var tutorial = "complete web development"
+// console.log(tutorial)
+
+// {
+//     var tutorial = "java script"
+//     {
+//         var tutorial = "python"
+//         {
+//             var tutorial = "Angular"
+//             console.log(tutorial)
+//         }
+//         console.log(tutorial)
+//     }
+//     console.log(tutorial)
+// }
+
+
+// let tutorial = "java script"
+// let tutorial = "python"        // you cannot redeclare same variable with let
+
+// {
+//     let tutorial = "java script"
+//     {
+//         let tutorial = "python"
+//         {
+//             let tutorial = "Angular"
+//             console.log(tutorial)
+//         }
+//         console.log(tutorial)
+//     }
+//     console.log(tutorial)
+// }
+
+
+// const tutorial = "java script"
+// const tutorial = "python"        // since it is constant we cannot redeclare the const variable
+
+{
+    const tutorial = "java script"
+    {
+        const tutorial = "python"
+        {
+            const tutorial = "Angular"
+            console.log(tutorial)
+        }
+        console.log(tutorial)
+    }
+    console.log(tutorial)
+}
