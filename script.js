4d93086b74b8c10eb62a14070891424c7da7e8a2
Madannagari Raju <madannagariraju@gmail.com>
2024-10-21 21:54:26 +0530

    second commit

--- script.js
+++ script.js
@@ -46,15 +46,172 @@
 // const tutorial = "java script"
 // const tutorial = "python"        // since it is constant we cannot redeclare the const variable
 
-{
-    const tutorial = "java script"
-    {
-        const tutorial = "python"
-        {
-            const tutorial = "Angular"
-            console.log(tutorial)
-        }
-        console.log(tutorial)
-    }
-    console.log(tutorial)
-}
+// {
+//     const tutorial = "java script"
+//     {
+//         const tutorial = "python"
+//         {
+//             const tutorial = "Angular"
+//             console.log(tutorial)
+//         }
+//         console.log(tutorial)
+//     }
+//     console.log(tutorial)
+// }
+
+
+
+//  NaN (Not a number) , null , undefined
+
+
+// let n = "raju"/2;
+// console.log(name)
+
+// let m = null;
+// console.log(m);
+
+// let v
+// console.log(v)
+
+// Understanding operators     ==  and ===
+
+// let a = 10 == 10
+// console.log(a) // true
+
+// let a2 = 10 === 10
+// console.log(a2) // true
+
+// let a3 = 10 == "10"
+// console.log(a) // true
+
+// let a4 = 10 === "10"
+// console.log(a4) // false
+
+
+// JavaScript dialog box functions 
+
+// let res = alert("Be, safe from virus attack")
+// console.log(res)  // undefined
+// let res2 = prompt("what is your name")
+// console.log(res2)
+// let res3 = confirm("is it you??");
+// console.log(res3) // OK -> true  Cancle -> false
+
+
+// Understanding Switch Case
+
+// let weekday = parseInt(prompt("Enter the week number"));
+
+
+// switch(weekday){
+//     case 1: alert("Monday")
+//             break
+//     case 2: alert("Tuesday")
+//             break
+//     case 3: alert("Wednesday")
+//             break
+//     case 4: alert("Thursday")
+//             break
+//     case 5: alert("Friday")
+//             break
+//     case 6: alert("Saturday")
+//             break
+//     case 7: alert("Sunday")
+//             break
+//     default : alert("Enter the valid week number")
+//             break   
+    
+// }
+
+
+//  pre-increment, post-increment, pre-decrement, post-decrement
+
+let n =10;
+// ++n;
+// console.log(n);  
+
+// console.log(++n)
+
+// n++;
+// console.log(n)
+
+//console.log(n++)
+
+// --n;
+// console.log(n)
+
+// console.log(--n)
+
+// n--;
+// console.log(n)
+
+// console.log(n--)
+
+
+//    functions 
+
+// function checkEvenOrOdd(n){
+//     if(n%2 ==0){
+//         return "Even"
+//     }
+//     return "Odd"
+// }
+
+// console.log(checkEvenOrOdd(2))
+// console.log(checkEvenOrOdd(3))
+// console.log(checkEvenOrOdd(4))
+
+
+//      list
+
+let favouriteFruits =["apples", "Kiwi", "oranges","graphes"];
+// console.log(typeof favouriteFruits)
+// console.log(favouriteFruits)
+
+// console.log(favouriteFruits[0])
+// console.log(favouriteFruits[1])
+// console.log(favouriteFruits[2])
+// console.log(favouriteFruits[3])
+
+// for(let i=0;i<favouriteFruits.length;i++){
+//     console.log(favouriteFruits[i]);
+// }
+    
+
+          // Array Methods --> push() pop(), unshift(), shift()
+
+// favouriteFruits.push("banana");
+// console.log(favouriteFruits)
+
+// let remove = favouriteFruits.pop();
+// console.log(remove)
+// console.log(favouriteFruits)
+
+// favouriteFruits.unshift("watermelon");
+// console.log(favouriteFruits)
+
+// let removefirst = favouriteFruits.shift();
+// console.log(removefirst)
+// console.log(favouriteFruits)
+
+
+
+//    Objects
+
+// let person = {
+//     name: "raju",
+//     age : 22,
+//     city : "hyd",
+//     favouriteFruits :["apples", "Kiwi", "oranges"],
+//     marksLevelWise:{
+//         tenth : 9.5,
+//         inter: 952,
+//         btech: 8.8
+//     }
+// }
+// console.log(person.name)
+// console.log(person['name'])
+
+// console.log(person.favouriteFruits[2])
+
+// console.log(person.marksLevelWise.inter)
